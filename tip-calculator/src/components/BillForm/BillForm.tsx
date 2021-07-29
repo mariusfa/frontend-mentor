e46e268b32fb13a88f40b2e2959626c7ce69e949
Marius Fagerland <mariusfagerland@yahoo.no>
2021-07-29 15:04:21 +0200

    Update inputs

--- tip-calculator/src/components/BillForm/BillForm.tsx
+++ tip-calculator/src/components/BillForm/BillForm.tsx
@@ -143,6 +143,7 @@ const BillForm: React.FC<Props> = ({ formData, setFormData, resetRef }) => {
                         css={customInputCss(isCustomTip)}
                         type='number'
                         id='customTip'
+                        name='customTip'
                         placeholder='Custom'
                         value={formData.customTip}
                         onChange={handleCustomTipChange}
@@ -160,6 +161,7 @@ const BillForm: React.FC<Props> = ({ formData, setFormData, resetRef }) => {
                 <input
                     css={inputCss}
                     id='numPeople'
+                    name='numPeople'
                     type='number'
                     onChange={handleFormChange}
                     value={formData.numPeople}
